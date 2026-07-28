ad6d95b953fe1a3004b2696734bddc36c699c2a6
reinhardworkspace <wahyureinhardseptiano@gmail.com>
2026-07-27 23:24:38 -0400

    Complete Emotion Detector project

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,51 @@
+import json
+import requests
+
+URL = (
+    "https://sn-watson-emotion.labs.skills.network/"
+    "v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+)
+
+HEADERS = {
+    "grpc-metadata-mm-model-id":
+    "emotion_aggregated-workflow_lang_en_stock"
+}
+
+
+def emotion_detector(text_to_analyze):
+    """
+    Analyze emotions from the provided text.
+    """
+
+    payload = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
+
+    response = requests.post(
+        URL,
+        headers=HEADERS,
+        json=payload,
+        timeout=30
+    )
+
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+
+    formatted_response = json.loads(response.text)
+
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+
+    dominant = max(emotions, key=emotions.get)
+
+    emotions["dominant_emotion"] = dominant
+
+    return emotions
